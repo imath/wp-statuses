2ebfe54a978a5de03d24b540cc5c603836bb058c
imath <math.viet@gmail.com>
2023-01-28 11:42:21 +0100

    2.1.6-alpha version bumps

--- wp-statuses.php
+++ wp-statuses.php
@@ -3,7 +3,7 @@
 Plugin Name: WP Statuses
 Plugin URI: https://imathi.eu/tag/wp-statuses/
 Description: Suggestions to improve the WordPress Post statuses API.
-Version: 2.1.5
+Version: 2.1.6-alpha
 Requires at least: 5.0
 Tested up to: 5.9
 License: GNU/GPL 2
@@ -69,7 +69,7 @@ final class WP_Statuses {
 	 */
 	private function setup_globals() {
 		// Version
-		$this->version = '2.1.5';
+		$this->version = '2.1.6-alpha';
 
 		// Domain
 		$this->domain = 'wp-statuses';
