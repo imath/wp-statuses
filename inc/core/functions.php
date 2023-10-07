963e4a2b5ce76d85225882dcee0cb81fe1b666ff
imath <math.viet@gmail.com>
2023-10-07 11:31:33 +0200

    Avoid the "Auto Draft" initial post title in Block Editor
    Fixes #67

--- inc/core/functions.php
+++ inc/core/functions.php
@@ -559,6 +559,13 @@ function wp_statuses_rest_prepare_for_response( WP_REST_Response $response, WP_P
 	// Always trick the Block Editor so that is uses the "Update" major action button.
 	if ( wp_statuses_is_post_editing() ) {
 		$post_type['status'] = 'private';
+
+		if ( isset( $post_type['title']['raw'] ) && __( 'Auto Draft', 'default' ) === $post_type['title']['raw'] ) {
+			$post_type['title'] = array(
+				'raw'      => '',
+				'rendered' => '',
+			);
+		}
 	}
 
 	$response->set_data( $post_type );
